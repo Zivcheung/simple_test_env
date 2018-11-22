1efe20dd7b235c5cd0aefffc78a1a1911199c150
TianzhuZhang <zivcheung@126.com>
2018-11-22 16:28:13 +0800

    first submit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import _ from "lodash";
+import Data from "./data.xml";
+import moment from "moment";
+import "./style.css"
+import Icon from "./image1.png";
+
+
+function component() {
+    var element = document.createElement('div');
+  
+    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+    element.innerHTML = _.join(['Hello', 'webpack',"font"], ' ');
+    element.classList.add("hello");
+    // 将图像添加到我们现有的 div。
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+    
+    console.log(Data);
+    // var time = document.createElement("div");
+    // var m = Moment.format("MMM Do YY");
+    moment.locale("en");
+    var time=moment().format("MMM D");
+    // var time = moment();
+    console.log(time);
+
+
+
+    return element;
+  }
+  
+  document.body.appendChild(component());
